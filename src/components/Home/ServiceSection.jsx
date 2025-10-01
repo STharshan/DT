849bcfce26fa95d120305fa605b98f0d531bf8a0
STharshan <sivatharshan81@gmail.com>
2025-10-01 17:57:05 +0530

    add

---
+++ src/components/Home/ServiceSection.jsx
@@ -0,0 +1,190 @@
+import React, { useRef, useEffect } from "react";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const services = [
+  {
+    title: "DT VALET",
+    desc: `Deep clean of all wheels including callipers, barrels and arches
+A two stage pre-wash including citrus wash and snow foam to lift off the top layer of dirt before contacting the paintwork
+Two bucket safe wash, one wash bucket and one rinse bucket so the mitt is clean every time it touches the car
+Soft bristle brush then cleans the tighter areas with APC
+The car is then dried with luxury drying towel and air blower
+Spray sealant is applied giving up to 8 weeks of protection`,
+    image: "/carpaint.avif",
+    path: "/services/mechanical",
+  },
+  {
+    title: "DT DETAIL",
+    desc: `2 Stage decontamination using fallout remover and tar & glue remover
+Car snow foamed again to remove any chemicals left behind
+Exterior wax upgraded giving up to 8 months protection
+All windows polished
+Exhaust tips cleaned and buffed
+​
+
+Deep interior clean using steam
+Mats and carpets shampooed and extracted
+All leather cleaned using leather solution with soft brushes then conditioned`,
+    image: "/gray.avif",
+    path: "/services/service",
+  },
+  {
+    title: "DT MAINTENANCE",
+    desc: `
+Once you have had a DT DETAIL you will automatically be enrolled on to the maintenance scheme
+
+This will maintain the condition of your car and eliminate the need of further details
+
+Maintenance cleans should be booked in within 8 weeks of a DT DETAIL and can then continue to be booked so long as it is within 8 weeks `,
+    image: "/carmechanic.avif",
+    path: "/services/mot",
+  },
+  {
+    title: "DT MACHINE POLISH",
+    desc: `
+The DT MACHINE POLISH is a full Stage 1 polish of the paintwork including wing mirrors, door pillars and grill
+
+​
+
+The aim is to remove between 70 and 90 percent of swirls and minor defects
+
+​
+
+2 Stage decontamination using fallout remover and tar & glue remover to get the paint as smooth as possible before the polishing stage
+Exterior wax upgraded giving up to 8 months protection
+All windows cleaned
+Exhaust tips cleaned and buffed
+Finished with tyre shine​
+Interior clean can be added as an optional added extra (see added extras below for more info and prices)`,
+    image: "/paint.webp",
+    path: "/services/wheel-alignment",
+  }
+];
+
+const ServiceSection = () => {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  }, []);
+
+  const scroll = (direction) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: direction === "left" ? -300 : 300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <section className="py-25 px-4 bg-white dark:bg-black relative transition-colors duration-300">
+      <div className="max-w-7xl mb-20 mx-auto relative">
+        {/* Section Title */}
+        <h2
+          className="text-center text-3xl md:text-4xl font-bold text-primary dark:text-primary mb-2 uppercase"
+          data-aos="fade-down"
+        >
+          Our Services
+        </h2>
+        <p
+          className="text-center text-light mb-8"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          From minor fixes to major repairs, we’ve got you covered.
+        </p>
+
+        <div className="relative overflow-hidden">
+          {/* Scroll Left Button */}
+          <button
+            onClick={() => scroll("left")}
+            className="absolute left-2 top-1/2 transform -translate-y-1/2 z-20
+              bg-black/50 dark:bg-white/20 hover:bg-primary dark:hover:bg-primary
+              text-white dark:text-white p-3 rounded-full shadow-md
+              transition-colors duration-300 group"
+            aria-label="Scroll Left"
+            data-aos="fade-right"
+            data-aos-delay="200"
+          >
+            <FaArrowLeft
+              size={20}
+              className="transition-transform duration-300 group-hover:-translate-x-2"
+            />
+          </button>
+
+          {/* Scroll Right Button */}
+          <button
+            onClick={() => scroll("right")}
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 z-20
+              bg-black/50 dark:bg-white/20 hover:bg-primary dark:hover:bg-primary
+              text-white dark:text-white p-3 rounded-full shadow-md
+              transition-colors duration-300 group"
+            aria-label="Scroll Right"
+            data-aos="fade-left"
+            data-aos-delay="200"
+          >
+            <FaArrowRight
+              size={20}
+              className="transition-transform duration-300 group-hover:translate-x-2"
+            />
+          </button>
+
+          {/* Scrollable Service Cards */}
+          <div
+            ref={scrollRef}
+            className="flex space-x-6 overflow-x-auto scroll-smooth px-10 py-20 scrollbar-hide"
+          >
+            {services.map((service, index) => (
+              <div
+                key={index}
+                className="relative min-w-[300px] md:min-w-[400px] h-[420px] rounded-lg shadow-lg border-primary border
+             hover:shadow-primary hover:border-primary/80 overflow-hidden group flex-shrink-0 border-primary hover:border-primary/80
+             hover:scale-105 active:scale-105 bg-white hover:shadow-primary border-primary hover:border-primary/80 dark:bg-black/90 transition-all duration-300"
+                data-aos="fade-up"
+                data-aos-delay={index * 150} // staggered animation for each card
+              >
+                {/* Image */}
+                <img
+                  src={service.image}
+                  alt={service.title}
+                  className="absolute inset-0 w-full h-full object-cover"
+                />
+
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-overlay dark:bg-overlay/70 hover:bg-overlay/60 dark:hover:bg-overlay/50 transition-all duration-300"></div>
+
+                {/* Text */}
+                <div className="absolute bottom-0 p-5 z-10">
+                  <h3 className="text-lg font-bold text-primary mb-2">
+                    {service.title}
+                  </h3>
+                  <p className="text-sm text-white mb-4">{service.desc}</p>
+                  <Link to={service.path}>
+                    <button className="mt-auto px-4 py-2 text-xs border border-light rounded-full font-semibold cursor-pointer
+                      bg-overlay dark:bg-overlay/30 hover:bg-primary dark:hover:bg-primary
+                      text-primary dark:text-primary hover:text-dark transition-all duration-300"
+                    >
+                      <FaArrowRight className="inline mr-2" />
+                      EXPLORE
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ServiceSection;
