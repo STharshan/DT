26a9022bac77faf3d0946007a7ea85e755e2188c
STharshan <sivatharshan81@gmail.com>
2025-10-02 18:55:25 +0530

    a

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -7,58 +7,27 @@ import "aos/dist/aos.css";
 const services = [
   {
     title: "DT VALET",
-    desc: `Deep clean of all wheels including callipers, barrels and arches
-A two stage pre-wash including citrus wash and snow foam to lift off the top layer of dirt before contacting the paintwork
-Two bucket safe wash, one wash bucket and one rinse bucket so the mitt is clean every time it touches the car
-Soft bristle brush then cleans the tighter areas with APC
-The car is then dried with luxury drying towel and air blower
-Spray sealant is applied giving up to 8 weeks of protection`,
+    desc: `Affordable, convenient cleaning to give your car a fresh showroom look.`,
     image: "/carpaint.avif",
     path: "/services/mechanical",
   },
   {
     title: "DT DETAIL",
-    desc: `2 Stage decontamination using fallout remover and tar & glue remover
-Car snow foamed again to remove any chemicals left behind
-Exterior wax upgraded giving up to 8 months protection
-All windows polished
-Exhaust tips cleaned and buffed
-​
-
-Deep interior clean using steam
-Mats and carpets shampooed and extracted
-All leather cleaned using leather solution with soft brushes then conditioned`,
+    desc: `Showroom shine that protects your paint and makes your car stand out.`,
     image: "/gray.avif",
     path: "/services/service",
   },
   {
     title: "DT MAINTENANCE",
     desc: `
-Once you have had a DT DETAIL you will automatically be enrolled on to the maintenance scheme
-
-This will maintain the condition of your car and eliminate the need of further details
-
-Maintenance cleans should be booked in within 8 weeks of a DT DETAIL and can then continue to be booked so long as it is within 8 weeks `,
+Once you have had a DT DETAIL you will automatically be enrolled on to the maintenance scheme`,
     image: "/carmechanic.avif",
     path: "/services/mot",
   },
   {
     title: "DT MACHINE POLISH",
     desc: `
-The DT MACHINE POLISH is a full Stage 1 polish of the paintwork including wing mirrors, door pillars and grill
-
-​
-
-The aim is to remove between 70 and 90 percent of swirls and minor defects
-
-​
-
-2 Stage decontamination using fallout remover and tar & glue remover to get the paint as smooth as possible before the polishing stage
-Exterior wax upgraded giving up to 8 months protection
-All windows cleaned
-Exhaust tips cleaned and buffed
-Finished with tyre shine​
-Interior clean can be added as an optional added extra (see added extras below for more info and prices)`,
+The DT MACHINE POLISH is a full Stage 1 polish of the paintwork including wing mirrors`,
     image: "/paint.webp",
     path: "/services/wheel-alignment",
   }
